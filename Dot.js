40531186dc64bc57ecfa0871c593e310d8b904d5
Shockysaket <75153447+Shockysaket@users.noreply.github.com>
2021-03-05 20:06:46 +0530

    Add files via upload

---
+++ Dot.js
@@ -0,0 +1,19 @@
+class Dot {
+    constructor(x,y){
+
+    
+    World.add(world, this.body);
+    if (this.rain.position  .y > height ) {
+        Matter.body .setPosition(this.rain,{x:random(0,400)  ,y:random(0,400)   })
+        
+}
+}
+  
+
+display () {
+
+for(var i=0; i<maxdrop;i++){
+   Drop.push  (new Createdrop(random(0,400)) (random(0,400)))
+
+}
+}
